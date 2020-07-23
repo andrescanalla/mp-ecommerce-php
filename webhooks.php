3c1f15a737ba8d57c44c51c32a1beec5d8df1821
wedelia <pasquinelli@a-w-a.com.ar>
2020-07-23 15:11:53 -0300

    hook

---
+++ webhooks.php
@@ -0,0 +1,12 @@
+<?php
+// Recibir el cuerpo de la petición.
+$input = @file_get_contents("php://input");
+// Parsear el contenido como JSON.
+$eventJson = json_decode($input);
+
+// Usar los datos del Webhooks para alguna acción.
+print $eventJson;
+
+// Responder
+http_response_code(200);
+?>
