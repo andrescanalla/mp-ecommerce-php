16f8521a477f8e974004b6ec2f60b45cd9341e19
wedelia <pasquinelli@a-w-a.com.ar>
2020-07-23 12:30:44 -0300

    fix

--- success.php
+++ success.php
@@ -4,10 +4,10 @@ $external_reference = $_GET['external_reference'];
 $collection_id = $_GET['collection_id'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.mercadopago.com/v1/payments/".$collection_id."?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-//curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);
-$jsonRes = json_decode($result);
+
 
 
 
@@ -507,7 +507,7 @@ $jsonRes = json_decode($result);
                                             El pago haya sido exitoso!!!
                                         </h2>
                                         <h3>payment_type:<?php echo $payment_type ?></h3>
-                                        <h3>payment_method_id:<?php echo $jsonRes->payment_method_id ?></h3>
+                                        <h3>payment_method_id:<?php echo ' ' ?></h3>
                                         <h3>external_reference:<?php echo $external_reference ?></h3>
                                         <h3>collection_id:<?php echo $collection_id ?></h3>
                                     </button>
