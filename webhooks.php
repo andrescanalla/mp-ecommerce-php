b66e1c7315e19a255261997a298767fc2e2cf4d8
wedelia <pasquinelli@a-w-a.com.ar>
2020-07-23 15:38:56 -0300

    debg

--- webhooks.php
+++ webhooks.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 // Recibir el cuerpo de la petición.
 $input = @file_get_contents("php://input");
 // Parsear el contenido como JSON.
@@ -6,7 +8,7 @@ $eventJson = json_decode($input);
 
 // Usar los datos del Webhooks para alguna acción.
 //print $eventJson;
-echo("JSON:".$eventJson);
+//echo("JSON:".$eventJson);
 
 // Responder
 http_response_code(200);
